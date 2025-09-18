9817702bbb71307d4b688b5128ca5f073fcb0af2
Naoki Nomoto <n@mode-mac-m3.local>
2025-09-18 15:27:23 +0900

    fix to print

--- lib.go
+++ lib.go
@@ -1,9 +1,11 @@
 package versioninggo
 
+import "fmt"
+
 func GetVersion() string {
 	return "v0.0.1"
 }
 
 func PrintVersion() string {
-	return "Version: " + GetVersion()
+	fmt.Println(GetVersion())
 }
